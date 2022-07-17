8fd287e3a3bb6f92d5363fd305afa89e52b8d4cf
Turza <cse.turza@gmail.com>
2022-07-17 17:45:29 +0600

    date time formats

--- csDateTime/Program.cs
+++ csDateTime/Program.cs
@@ -17,6 +17,17 @@ namespace csDateTime
             Console.WriteLine("Hour: "+ now.Hour);
             Console.WriteLine("Minutes: " + now.Minute);
 
+            Console.WriteLine();
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine("ToLongDateString: " + now.ToLongDateString());
+            Console.WriteLine("ToShortDateString: " + now.ToShortDateString());
+            Console.WriteLine("ToLongTimeString: " + now.ToLongTimeString());
+            Console.WriteLine("ToShortTimeString: " + now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+
         }
     }
 }
