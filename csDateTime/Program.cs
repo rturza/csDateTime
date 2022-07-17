bac9061e313d46dace551d72cf548ad4bcd08169
Turza <cse.turza@gmail.com>
2022-07-17 17:33:04 +0600

    Add project files.

---
+++ csDateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csDateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime();
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: "+ now.Hour);
+            Console.WriteLine("Minutes: " + now.Minute);
+
+        }
+    }
+}
